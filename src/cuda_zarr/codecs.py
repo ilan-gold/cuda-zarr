aaaf1002ac56bda6d656ee467ef20d3d4ce0d763
Ilan Gold <ilanbassgold@gmail.com>
2025-02-24 19:05:39 +0000

    (fix): decde/encode properly

--- src/cuda_zarr/codecs.py
+++ src/cuda_zarr/codecs.py
@@ -1,7 +1,10 @@
+import asyncio
 from functools import cached_property
 
 import zarr
 from kvikio.nvcomp_codec import NvCompBatchCodec
+from zarr.core.array_spec import ArraySpec
+from zarr.core.buffer import Buffer
 
 
 class ZstdGPU(zarr.codecs.ZstdCodec):
@@ -18,3 +21,23 @@ class ZstdGPU(zarr.codecs.ZstdCodec):
     @cached_property
     def _zstd_codec(self) -> NvCompBatchCodec:
         return NvCompBatchCodec("Zstd")
+
+    async def _decode_single(
+        self,
+        chunk_bytes: Buffer,
+        chunk_spec: ArraySpec,
+    ) -> Buffer:
+        b = await asyncio.to_thread(
+            self._zstd_codec.decode, chunk_bytes.as_array_like()
+        )
+        return chunk_spec.prototype.buffer.from_array_like(b.ravel().astype("int8"))
+
+    async def _encode_single(
+        self,
+        chunk_bytes: Buffer,
+        chunk_spec: ArraySpec,
+    ) -> Buffer | None:
+        b = await asyncio.to_thread(
+            self._zstd_codec.encode, chunk_bytes.as_array_like()
+        )
+        return chunk_spec.prototype.buffer.from_bytes(b)
