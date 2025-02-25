0f46071098dff4c6b9ca1628d6616802619652b8
Ilan Gold <ilanbassgold@gmail.com>
2025-02-25 08:04:53 +0100

    (fix): decde/encode properly (#3)

--- src/cuda_zarr/codecs.py
+++ src/cuda_zarr/codecs.py
@@ -1,7 +1,10 @@
+from collections.abc import Iterable
 from functools import cached_property
 
 import zarr
 from kvikio.nvcomp_codec import NvCompBatchCodec
+from zarr.core.array_spec import ArraySpec
+from zarr.core.buffer import Buffer
 
 
 class ZstdGPU(zarr.codecs.ZstdCodec):
@@ -18,3 +21,41 @@ class ZstdGPU(zarr.codecs.ZstdCodec):
     @cached_property
     def _zstd_codec(self) -> NvCompBatchCodec:
         return NvCompBatchCodec("Zstd")
+
+    async def decode(
+        self,
+        chunks_and_specs: Iterable[tuple[Buffer | None, ArraySpec]],
+    ) -> Iterable[Buffer | None]:
+        chunks_and_specs_list = [cs for cs in chunks_and_specs]
+        input_bytes = [
+            chunk_bytes.as_array_like()
+            for (chunk_bytes, _) in chunks_and_specs_list
+            if chunk_bytes is not None
+        ]
+        iterable_decoded = iter(self._zstd_codec.decode_batch(input_bytes))
+        return [
+            chunk_spec.prototype.buffer.from_array_like(
+                next(iterable_decoded).astype("int8")
+            )
+            if chunk_bytes is not None
+            else None
+            for (chunk_bytes, chunk_spec) in chunks_and_specs_list
+        ]
+
+    async def encode(
+        self,
+        chunks_and_specs: Iterable[tuple[Buffer | None, ArraySpec]],
+    ) -> Iterable[Buffer | None]:
+        chunks_and_specs_list = [cs for cs in chunks_and_specs]
+        input_bytes = [
+            chunk_bytes.as_array_like()
+            for (chunk_bytes, _) in chunks_and_specs_list
+            if chunk_bytes is not None
+        ]
+        iterable_encoded = iter(self._zstd_codec.encode_batch(input_bytes))
+        return [
+            chunk_spec.prototype.buffer.from_bytes(next(iterable_encoded))
+            if chunk_bytes is not None
+            else None
+            for (chunk_bytes, chunk_spec) in chunks_and_specs_list
+        ]
